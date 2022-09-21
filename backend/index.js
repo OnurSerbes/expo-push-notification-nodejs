a067bac697b7d18d84785dc5dbf7617637b53d2b
onur.serbes <onur.serbes@dt.net.tr>
2022-09-21 13:55:44 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,84 @@
+const { Expo } = require("expo-server-sdk");
+
+// Create a new Expo SDK client
+// optionally providing an access token if you have enabled push security
+let expo = new Expo({ accessToken: process.env.EXPO_ACCESS_TOKEN });
+
+// Create the messages that you want to send to clients
+let messages = [];
+// Add your usertoken database instead of directly typing the tokens
+let somePushTokens = ["ExponentPushToken[jA7ugBLr_B0Qq6wtDqvssE]"];
+for (let pushToken of somePushTokens) {
+  // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+  // Check that all your push tokens appear to be valid Expo push tokens
+  if (!Expo.isExpoPushToken(pushToken)) {
+    console.error(`Push token ${pushToken} is not a valid Expo push token`);
+    continue;
+  }
+  // Construct a message
+  messages.push({
+    to: pushToken,
+    sound: "default",
+    title: "NodeJS",
+    body: "This is a test notification from backend",
+    data: { withSome: "data" },
+  });
+}
+
+let chunks = expo.chunkPushNotifications(messages);
+let tickets = [];
+(async () => {
+  // Send the chunks to the Expo push notification service.
+  for (let chunk of chunks) {
+    try {
+      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+      console.log(ticketChunk);
+      tickets.push(...ticketChunk);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+})();
+
+// The ID of each receipt is sent back in the response "ticket" for each
+// notification. In summary, sending a notification produces a ticket, which
+// contains a receipt ID you later use to get the receipt.
+//
+// The receipts may contain error codes to which you must respond. In
+// particular, Apple or Google may block apps that continue to send
+// notifications to devices that have blocked notifications or have uninstalled
+let receiptIds = [];
+for (let ticket of tickets) {
+  // NOTE: Not all tickets have IDs; for example, tickets for notifications
+  // that could not be enqueued will have error information and no receipt ID.
+  if (ticket.id) {
+    receiptIds.push(ticket.id);
+  }
+}
+
+let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+(async () => {
+  for (let chunk of receiptIdChunks) {
+    try {
+      let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+      console.log(receipts);
+
+      for (let receiptId in receipts) {
+        let { status, message, details } = receipts[receiptId];
+        if (status === "ok") {
+          continue;
+        } else if (status === "error") {
+          console.error(
+            `There was an error sending a notification: ${message}`
+          );
+          if (details && details.error) {
+            console.error(`The error code is ${details.error}`);
+          }
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+})();
---
+++ mobile/App.js
@@ -0,0 +1,124 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+import { useState, useEffect, useRef } from "react";
+
+//notification settings
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
+export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    //token setup
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    //notification response
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    //delete the unnececarily notification
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  //for testing
+  async function sendPushNotification(expoPushToken) {
+    const message = {
+      to: expoPushToken,
+      sound: "default",
+      title: "Original Title",
+      body: "And here is the body!",
+      data: { someData: "goes here" },
+    };
+
+    await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Accept-encoding": "gzip, deflate",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    });
+  }
+
+  //returns the token
+  async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === "android") {
+      await Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+
+    return token;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Expo Notificaiton App</Text>
+      <Button
+        title="Notfiy Me !"
+        onPress={async () => {
+          await sendPushNotification(expoPushToken);
+        }}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
